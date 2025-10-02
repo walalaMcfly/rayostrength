3b94231b6f591089605e671fe524e32224602158
Fabian <127862887+iam-Fab@users.noreply.github.com>
2025-10-02 09:52:53 -0300

    creacion de branch y styles
    styles globsles

---
+++ styles/global.js
@@ -0,0 +1,36 @@
+// // src/styles/global.js
+// import { StyleSheet } from "react-native";
+
+// export const globalStyles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "#fff",
+//     padding: 16,
+//   },
+//   title: {
+//     fontSize: 20,
+//     fontWeight: "bold",
+//     color: "#333",
+//   },
+//   text: {
+//     fontSize: 16,
+//     color: "#555",
+//   },
+// });
+
+
+
+
+
+//para importarlo en cualquier componente usar el sgte codigo:
+
+// import { globalStyles } from "../styles/global";
+
+// export default function Home() {
+//   return (
+//     <View style={globalStyles.container}>
+//       <Text style={globalStyles.title}>Bienvenido</Text>
+//       <Text style={globalStyles.text}>Este es un texto global</Text>
+//     </View>
+//   );
+// }
