48afbf5af7848796792b229c9e139cf9a143abf4
walalaMcfly <129227549+walalaMcfly@users.noreply.github.com>
2025-11-19 00:17:34 -0300

    Cambios cors

--- backend/server.js
+++ backend/server.js
@@ -22,11 +22,14 @@ app.use(cors({
     "http://localhost:3000",
     "https://localhost:3000",
     "http://localhost:5173",
-    "https://localhost:5173"
+    "https://localhost:5173",
+    "http://localhost:8081", 
+    "http://127.0.0.1:8081", 
+    "http://0.0.0.0:8081"    
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+  allowedHeaders: ['Content-Type', 'Authorization','Accept']
 }));
 
 app.use(express.json());
